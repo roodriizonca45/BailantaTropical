ed3df3650067b2103856e982cc4c1dda0aff339f
roodriizonca45 <roodriizonca45@gmail.com>
2026-06-05 18:08:30 -0300

    lib

--- lib/db.ts
+++ lib/db.ts
@@ -3,6 +3,7 @@ import { Pool } from "pg";
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
+  options: '-c search_path=public',
 });
 
 export default pool;
